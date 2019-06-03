453cf06714c7544d4139527bad3f1f8c63e418ad
Adam Ervin <adamjervin@gmail.com>
2019-06-03 20:09:15 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -25,8 +25,10 @@ def end_game(number)
 end
 
 def initial_round
- card_total = deal_card + deal_card
- display_card_total(card_total)
+ card_one = deal_card
+  card_two = deal_card
+  card_total = card_one + card_two
+  display_card_total(card_total)
  
   
 end
